037baf2ab64647e4b68a7139800b20f08d26c561
Viacheslav <viacheslavkukurika@gmail.com>
2026-04-19 12:32:28 +0300

    feat: homework 11 (subtask 4)

--- lib/lesson_11/homework_11_screen.dart
+++ lib/lesson_11/homework_11_screen.dart
@@ -14,106 +14,108 @@ class Homework11Screen extends StatelessWidget {
           style: TextStyle(fontSize: 19, fontWeight: FontWeight.bold),
         ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              width: 300,
-              height: 150,
-              decoration: BoxDecoration(
-                color: const Color(0xFF096EEA),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Icon(Icons.star, color: Color(0xFFFFFD71)),
-                      SizedBox(width: 10),
-                      Text(
-                        'Привіт, Flutter!',
-                        style: TextStyle(
-                          color: Color(0xFFFFFFFF),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF096EEA),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Icon(Icons.star, color: Color(0xFFFFFD71)),
+                          SizedBox(width: 10),
+                          Text(
+                            'Привіт, Flutter!',
+                            style: TextStyle(
+                              color: Color(0xFFFFFFFF),
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(width: 10),
+                          Icon(Icons.star, color: Color(0xFFFFFD71)),
+                        ],
                       ),
-                      SizedBox(width: 10),
-                      Icon(Icons.star, color: Color(0xFFFFFD71)),
-                    ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              width: 300,
-              height: 150,
-              decoration: BoxDecoration(
-                color: const Color(0xFF08E900),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Icon(Icons.star, color: Color(0xFFFFFD71)),
-                      SizedBox(width: 10),
-                      Text(
-                        'Привіт, Flutter!',
-                        style: TextStyle(
-                          color: Color(0xFFFFFFFF),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+              const SizedBox(height: 8),
+              Container(
+                width: 300,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: const Color(0xFF08E900),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(Icons.star, color: Color(0xFFFFFD71)),
+                        SizedBox(width: 10),
+                        Text(
+                          'Привіт, Flutter!',
+                          style: TextStyle(
+                            color: Color(0xFFFFFFFF),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 10),
-                      Icon(Icons.star, color: Color(0xFFFFFD71)),
-                    ],
+                        SizedBox(width: 10),
+                        Icon(Icons.star, color: Color(0xFFFFFD71)),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Container(
-              width: 300,
-              height: 150,
-              decoration: BoxDecoration(
-                color: const Color(0xFFEA090C),
-                borderRadius: BorderRadius.circular(16),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: const [
-                      Icon(Icons.star, color: Color(0xFFFFFD71)),
-                      SizedBox(width: 10),
-                      Text(
-                        'Привіт, Flutter!',
-                        style: TextStyle(
-                          color: Color(0xFFFFFFFF),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+              const SizedBox(height: 8),
+              Container(
+                width: 300,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFEA090C),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: const [
+                        Icon(Icons.star, color: Color(0xFFFFFD71)),
+                        SizedBox(width: 10),
+                        Text(
+                          'Привіт, Flutter!',
+                          style: TextStyle(
+                            color: Color(0xFFFFFFFF),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(width: 10),
-                      Icon(Icons.star, color: Color(0xFFFFFD71)),
-                    ],
+                        SizedBox(width: 10),
+                        Icon(Icons.star, color: Color(0xFFFFFD71)),
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
