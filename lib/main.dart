b52245b2c0e6c792b368be257af51722a224cad3
Viacheslav <viacheslavkukurika@gmail.com>
2026-04-22 11:50:54 +0300

    chore: update analyzer settings and clean test files
    :wq

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,89 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hw/lesson_11/homework_11_screen.dart';
+import 'package:flutter_hw/widgets_main_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const FlutterWidgetsApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class FlutterWidgetsApp extends StatelessWidget {
+  const FlutterWidgetsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const Homework11Screen(),
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Lab'),
+        backgroundColor: Colors.blue.shade100,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            FeatureCard(
+              title: 'Widgets',
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute<Widget>(
+                  builder: (context) => const WidgetsScreen(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class FeatureCard extends StatelessWidget {
+  const FeatureCard({
+    required this.title,
+    required this.onTap,
+    super.key,
+  });
+
+  final String title;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 2,
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              Icon(
+                Icons.arrow_forward_ios,
+                size: 16,
+                color: Colors.grey.shade600,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
